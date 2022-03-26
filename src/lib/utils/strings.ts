b30c05a2a44ac23df85e51c23c46946001c1b2df
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2022-03-26 12:54:28 +0200

    Add strings util

---
+++ src/lib/utils/strings.ts
@@ -0,0 +1,19 @@
+export const capitalize = (content: string): string => content.charAt(0).toUpperCase() + content.slice(1).toLowerCase()
+
+export const removeHTML = (content: string): string => {
+  let filteredContent = content
+  if (filteredContent) {
+    const removes = ['<strong>', '</strong>', '<i>', '</i>']
+
+    removes.forEach((remove) => {
+        filteredContent = filteredContent.replace(new RegExp(remove, 'g'), '')
+    })
+  }
+
+  return filteredContent
+}
+
+export default {
+  capitalize,
+  removeHTML,
+}
---
+++ tests/unit/lib/utils/strings.test.ts
@@ -0,0 +1,17 @@
+import { capitalize, removeHTML } from 'lib/utils/strings'
+
+describe('String utils tests', () => {
+  test('Capitalizes text', () => {
+    expect(capitalize('ja nyt kun mä meen')).toBe('Ja nyt kun mä meen')
+    expect(capitalize('MITÄÄN KADU EN')).toBe('Mitään kadu en')
+  })
+
+  test('Removes html', () => {
+    expect(removeHTML('<strong>Strong</strong> <i>italics</i>')).toBe('Strong italics')
+    expect(removeHTML('<strong>Strong</strong> <strong>Strong</strong>')).toBe('Strong Strong')
+  })
+
+  test('Returns incorrect content as-is', () => {
+    expect(removeHTML(null as unknown as string)).toBeNull()
+  })
+})
