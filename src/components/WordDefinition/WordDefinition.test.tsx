f8b17e3eb79d82df7b2c536035222edebd03bbe8
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2022-04-02 17:04:58 +0300

    Add WordDefinition component

---
+++ src/components/WordDefinition/WordDefinition.test.tsx
@@ -0,0 +1,43 @@
+import ReactDOM from 'react-dom'
+import renderer from 'react-test-renderer'
+import WordDefinition from './index'
+
+const word = {
+  word: 'þrályndi',
+  partOfSpeech: 'n',
+  definition: 'þrályndi, n. d. s.; hélt honum aptr fornsiðvenja ok mykyt þrályndi Barl. 12524;Fgrdl. 5816.',
+  slug: 'thralyndi',
+}
+
+const abbreviations = [
+  {
+    abbreviation: 'n.',
+    explanation: 'Neutrum.',
+  },
+  {
+    abbreviation: 's.',
+    explanation: 'substantiv.',
+  },
+]
+
+describe('WordDefinition component', () => {
+  test('Does not crash', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<WordDefinition data={word} abbreviations={abbreviations} />, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
+
+  test('Matches snapshot', () => {
+    const tree = renderer.create(
+      <WordDefinition data={word} abbreviations={abbreviations} />,
+    ).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  test('Has correct label', () => {
+    const tree = renderer.create(<WordDefinition data={word} abbreviations={abbreviations} />)
+    const { root } = tree
+
+    expect(root.findByType('h1').children).toEqual(['Þrályndi'])
+  })
+})
---
+++ src/components/WordDefinition/index.tsx
@@ -0,0 +1,47 @@
+import { capitalize } from 'lib/utils/strings'
+import { lettersToRunes } from 'younger-futhark'
+import { Abbreviation, addAbbreviationsToContent } from 'lib/services/abbreviations'
+import { DictionaryEntry } from 'lib/models/dictionary'
+import Abbreviations from 'components/Abbreviations'
+import styles from './WordDefinition.module.scss'
+
+interface WordDefinitionProps{
+  data: DictionaryEntry,
+  abbreviations: Abbreviation[]
+}
+
+export default function WordDefinition({ data, abbreviations }: WordDefinitionProps) {
+  const { word, partOfSpeech, definition } = data
+
+  return (
+    <article className={styles.section}>
+      <header>
+        <h1 lang="non">{capitalize(word)}</h1>
+
+        <small className={styles.subHeading}>
+          Old Norwegian Dictionary - {word.toLowerCase()}
+        </small>
+        <p>Meaning of Old Norwegian word <em>&quot;{word}&quot;</em></p>
+      </header>
+
+      <dl className={styles.definitionList}>
+        <dt><strong>{word}</strong></dt>
+        <dd
+          lang="nor"
+          className={styles.itemDescription}
+          dangerouslySetInnerHTML={{
+            __html: addAbbreviationsToContent(definition, abbreviations),
+          } }
+        ></dd>
+      </dl>
+
+      <p><strong>Part of speech:</strong> <em>{partOfSpeech}</em></p>
+
+      <p>Possible runic inscription in <em>Younger Futhark:</em>
+        <span className={styles.rune}>{ lettersToRunes(word) }</span>
+      </p>
+
+      <Abbreviations abbreviations={abbreviations} />
+    </article>
+  )
+}
