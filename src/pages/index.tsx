076316e6fc0b2aa0f4ae0963b724fa8e83b73a31
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2022-10-01 12:16:42 +0300

    Frontpage: add written language section
    Closes #137

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,11 +30,22 @@ export default function Index({ letters }: IndexProps) {
         dictionary by Johan Fritzner, originally published in the late 1800s</p>
 
         <p>The dictionary contains over 40 000 translations
-          from Old Norwegian/Norse to Norwegian</p>
+          from Old Norwegian/Norse to Norwegian. This is the largest dictionary of the language.</p>
 
         <Link href="/search">
          <a className="button">Search the dictionary</a>
         </Link>
+      </ContentArea>
+
+      <ContentArea>
+        <h2 className="h3">Written language of the dictionary</h2>
+        <p>The dictionary volumes were published between 1862 and 1896.
+          This predates current written standars of Norwegian, namely Bokmål and Nynorsk.
+        </p>
+
+        <p>Therefore, the written definitions may seem old fashioned or more dano-norwegian
+          than one would expect. The writing system used is probably Riksmål.
+        </p>
       </ContentArea>
     </Layout>
   )
