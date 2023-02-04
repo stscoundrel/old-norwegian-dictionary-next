927c51b01f4d9505a9ec56b84e61da147d2da69e
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2023-02-04 15:57:25 +0200

    Add crosslinks component

---
+++ src/components/Crosslinks/index.tsx
@@ -0,0 +1,45 @@
+import { Crosslink } from 'lib/services/crosslinks'
+import ExternalLink from 'components/ExternalLink'
+import styles from './Crosslinks.module.scss'
+
+interface CrossLinkProps {
+  crosslinks: Crosslink[]
+}
+
+export default function Crosslinks({ crosslinks }: CrossLinkProps) {
+  const getRelatedDictionaryName = (source: string): string => {
+    if (source === 'old-norse') {
+      return 'Old Norse - Cleasby & Vigfusson Dictionary'
+    }
+
+    if (source === 'old-swedish') {
+      return 'Old Swedish - K.F Söderwall\'s Dictionary'
+    }
+
+    if (source === 'old-icelandic') {
+      return 'Old Icelandic - Geir Zoëga\'s Dictionary'
+    }
+
+    return ''
+  }
+
+  if (crosslinks.length === 0) {
+    return null
+  }
+
+  return (
+    <>
+      <h4>Also available in related dictionaries:</h4>
+      <p>This headword also appears in dictionaries of other languages related
+        to Old Norwegian.
+      </p>
+      <ul className={styles.list}>
+        {crosslinks.map(({ url, source }, index) => (
+          <li className={styles.listItem} key={`crosslink-${index}`}>
+            <ExternalLink href={url} title={getRelatedDictionaryName(source)} />
+          </li>
+        ))}
+      </ul>
+    </>
+  )
+}
