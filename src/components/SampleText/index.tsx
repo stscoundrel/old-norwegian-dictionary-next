9f0392cd80faa36c8f8b6aeda7eaaa039863f668
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2023-02-05 19:59:28 +0200

    Add sample text of Old Norwegian
    Closes #203

---
+++ src/components/SampleText/index.tsx
@@ -0,0 +1,23 @@
+export default function SampleText() {
+  return (
+    <>
+      <p className="h4">
+        A sample of Old Norwegian:
+      </p>
+      <p>
+        <em>
+            Magnus með guðs miskun noregs konungr son Hakonar konungs sonar son Suerris konungs
+            sender ollum gudes vínum ok sínum j frosto þings logum.
+            Qveðju Guðs ok sina Þer vitið at hínír skynsamozsto menn af frosto þings logum hafa
+            iðurlega getet firir oss at þer hafet spurt at ver
+            hofum lut i at t bøta nokot um flæstar logbøkr.
+        </em>
+      </p>
+
+      <p>
+        - Excerpt from Landslova, written between 1300 and 1320.
+      </p>
+      <hr />
+    </>
+  )
+}
