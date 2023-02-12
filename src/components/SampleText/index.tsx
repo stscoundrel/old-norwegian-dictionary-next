de2d2ff672e19683a33958a5f25be3229d3dcb45
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2023-02-12 11:02:05 +0200

    Sample text: add links to entries
    Closes #211

--- src/components/SampleText/index.tsx
+++ src/components/SampleText/index.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 export default function SampleText() {
   return (
     <>
@@ -6,11 +8,10 @@ export default function SampleText() {
       </p>
       <p>
         <em>
-            Magnus með guðs miskun noregs konungr son Hakonar konungs sonar son Suerris konungs
-            sender ollum gudes vínum ok sínum j frosto þings logum.
-            Qveðju Guðs ok sina Þer vitið at hínír skynsamozsto menn af frosto þings logum hafa
-            iðurlega getet firir oss at þer hafet spurt at ver
-            hofum lut i at t bøta nokot um flæstar logbøkr.
+            Magnus <Link href="/word/med">með</Link> <Link href="/word/gud">guðs</Link> <Link href="/word/miskunn">miskun</Link> <Link href="/word/noregr">noregs</Link> <Link href="konungr">konungr</Link> <Link href="son">son</Link> Hakonar <Link href="konungr">konungs</Link> <Link href="sonarsonr">sonar son</Link> Suerris <Link href="konungr">konungs</Link> <Link href="/word/senda">sender</Link> ollum gudes vínum ok sínum j frosto <Link href="/word/thing">þings</Link> <Link href="/word/log">logum</Link>.
+            <Link href="/word/kvedja">Qveðju</Link> <Link href="/word/gud">Guðs</Link> <Link href="/word/ok">ok</Link> <Link href="/word/sina">sina</Link> Þer <Link href="/word/vita">vitið</Link> at hínír skynsamozsto menn af frosto <Link href="/word/thing">þings</Link> <Link href="/word/log">logum</Link> <Link href="/word/hafa">hafa</Link> iðurlega
+            getet <Link href="/word/firir">firir</Link> <Link href="/word/oss-2">oss</Link> at þer hafet <Link href="/word/spyrja">spurt</Link> at ver
+            hofum lut i at t bøta nokot um flæstar <Link href="/word/logbok">logbøkr</Link>.
         </em>
       </p>
 
