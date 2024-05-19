09fef1faee375b18b3cc73376e32dc8c889b8826
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2024-05-19 10:01:23 +0300

    Disable link prefetch
    Closes #258

--- src/components/Breadcrumbs/index.tsx
+++ src/components/Breadcrumbs/index.tsx
@@ -45,7 +45,7 @@ export default function Breadcrumbs({ type, content }: BreadcrumbProps) {
     <nav className={styles.section}>
       <div className="container">
         {breadcrumbs.map(({ label, url }) => (
-          <Link key={url} href={url} className={styles.link}>
+          <Link key={url} href={url} className={styles.link} prefetch={false}>
             {label}
           </Link>
         ))}
--- src/components/SampleText/index.tsx
+++ src/components/SampleText/index.tsx
@@ -8,10 +8,10 @@ export default function SampleText() {
       </p>
       <p>
         <em>
-            Magnus <Link href="/word/med">með</Link> <Link href="/word/gud">guðs</Link> <Link href="/word/miskunn">miskun</Link> <Link href="/word/noregr">noregs</Link> <Link href="/word/konungr">konungr</Link> <Link href="/word/son">son</Link> Hakonar <Link href="/word/konungr">konungs</Link> <Link href="/word/sonarsonr">sonar son</Link> Suerris <Link href="/word/konungr">konungs</Link> <Link href="/word/senda">sender</Link> ollum gudes vínum ok sínum j frosto <Link href="/word/thing">þings</Link> <Link href="/word/log">logum</Link>.
-            <Link href="/word/kvedja">Qveðju</Link> <Link href="/word/gud">Guðs</Link> <Link href="/word/ok">ok</Link> <Link href="/word/sina">sina</Link> Þer <Link href="/word/vita">vitið</Link> at hínír skynsamozsto menn af frosto <Link href="/word/thing">þings</Link> <Link href="/word/log">logum</Link> <Link href="/word/hafa">hafa</Link> iðurlega
-            getet <Link href="/word/firir">firir</Link> <Link href="/word/oss-2">oss</Link> at þer hafet <Link href="/word/spyrja">spurt</Link> at ver
-            hofum lut i at t bøta nokot um flæstar <Link href="/word/logbok">logbøkr</Link>.
+            Magnus <Link href="/word/med" prefetch={false}>með</Link> <Link href="/word/gud" prefetch={false}>guðs</Link> <Link href="/word/miskunn" prefetch={false}>miskun</Link> <Link href="/word/noregr" prefetch={false}>noregs</Link> <Link href="/word/konungr" prefetch={false}>konungr</Link> <Link href="/word/son" prefetch={false}>son</Link> Hakonar <Link href="/word/konungr" prefetch={false}>konungs</Link> <Link href="/word/sonarsonr" prefetch={false}>sonar son</Link> Suerris <Link href="/word/konungr" prefetch={false}>konungs</Link> <Link href="/word/senda" prefetch={false}>sender</Link> ollum gudes vínum ok sínum j frosto <Link href="/word/thing" prefetch={false}>þings</Link> <Link href="/word/log" prefetch={false}>logum</Link>.
+            <Link href="/word/kvedja" prefetch={false}>Qveðju</Link> <Link href="/word/gud" prefetch={false}>Guðs</Link> <Link href="/word/ok" prefetch={false}>ok</Link> <Link href="/word/sina" prefetch={false}>sina</Link> Þer <Link href="/word/vita" prefetch={false}>vitið</Link> at hínír skynsamozsto menn af frosto <Link href="/word/thing" prefetch={false}>þings</Link> <Link href="/word/log" prefetch={false}>logum</Link> <Link href="/word/hafa" prefetch={false}>hafa</Link> iðurlega
+            getet <Link href="/word/firir" prefetch={false}>firir</Link> <Link href="/word/oss-2" prefetch={false}>oss</Link> at þer hafet <Link href="/word/spyrja" prefetch={false}>spurt</Link> at ver
+            hofum lut i at t bøta nokot um flæstar <Link href="/word/logbok" prefetch={false}>logbøkr</Link>.
         </em>
       </p>
 
